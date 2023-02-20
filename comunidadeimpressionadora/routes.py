94ce7f2c6312982ca2ca3690ddc1254476b804ab
Guilherme Montenegro Ferreira <69219345+MF-Guilherme@users.noreply.github.com>
2023-02-20 19:07:44 -0300

    Add files via upload

---
+++ comunidadeimpressionadora/routes.py
@@ -0,0 +1,166 @@
+from flask import render_template, request, flash, redirect, url_for, abort
+from comunidadeimpressionadora import app, database, bcrypt
+from comunidadeimpressionadora.forms import FormLogin, FormCriarConta, FormEditarPerfil, FormCriarPost
+from comunidadeimpressionadora.models import Usuario, Post
+from flask_login import login_user, logout_user, current_user, login_required
+import secrets
+import os
+from PIL import Image
+
+
+@app.route("/")
+def home():
+    posts = Post.query.order_by(Post.id.desc())
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/contato")
+def contato():
+    return render_template('contato.html')
+
+
+@app.route('/usuarios')
+@login_required
+def usuarios():
+    lista_usuarios = Usuario.query.all()
+    return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form_login = FormLogin()
+    form_criarconta = FormCriarConta()
+    # se o formulario foi validado ao submeter E o botão for o 'botao de login' (utilizando o AND pq temos 2 forms na mesma página
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+        usuario = Usuario.query.filter_by(email=form_login.email.data).first()
+        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+            login_user(usuario, remember=form_login.lembrar_dados.data)
+            # exibir mensagem de login bem sucedido
+            flash(f'login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
+            parametro_next = request.args.get('next')
+            if parametro_next:
+                return redirect(parametro_next)
+            else:
+                # redirecionar para a homepage
+                return redirect(url_for('home'))
+        else:
+            flash(f'Falha no login. E-mail ou Senha incorretos.', 'alert-danger')
+    if form_criarconta.validate_on_submit() and 'botao_submit_criar_conta' in request.form:
+        # instanciar o Usuario
+        senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data)
+        usuario = Usuario(username=form_criarconta.username.data, email=form_criarconta.email.data, senha=senha_cript)
+        # adicionar a sessão
+        database.session.add(usuario)
+        # commit na sessão
+        database.session.commit()
+        flash(f'Conta criada para o e-mail: {form_criarconta.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+    return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
+
+
+@app.route('/sair')
+@login_required
+def sair():
+    logout_user()
+    flash(f'Logout feito com sucesso!', 'alert-success')
+    return redirect(url_for('login'))
+
+
+@app.route('/perfil')
+@login_required
+def perfil():
+    foto_perfil = url_for('static', filename=f'fotos_perfil/{current_user.foto_perfil}')
+    return render_template('perfil.html', foto_perfil=foto_perfil)
+
+
+@app.route('/post/criar', methods=['GET', 'POST'])
+@login_required
+def criar_post():
+    form = FormCriarPost()
+    if form.validate_on_submit():
+        post = Post(titulo=form.titulo.data, corpo=form.corpo.data, autor=current_user)
+        database.session.add(post)
+        database.session.commit()
+        flash('Post criado com sucesso!', 'alert-success')
+        return redirect(url_for('home'))
+    return render_template('criarpost.html', form=form)
+
+
+def salvar_imagem(imagem):
+    # adicionar um codigo aleatorio no nome da imagem
+    codigo = secrets.token_hex(8)  # criei o código aleatorio
+    nome, extensao = os.path.splitext(imagem.filename) # separei o nome da extensão
+    nome_arquivo = nome + codigo + extensao  # juntei nome, codigo aleatório e extensão
+    caminho_completo = os.path.join(app.root_path, 'static/fotos_perfil', nome_arquivo)  # criei o path completo para salvar
+
+    # reduzir o tamanho da imagem
+    tamanho = (500, 500)  # 200 x 200
+    imagem_reduzida = Image.open(imagem)
+    imagem_reduzida.thumbnail(tamanho)
+
+    # salvar a imagem no caminho completo '/static/fotos_perfil'
+    imagem_reduzida.save(caminho_completo)
+    return nome_arquivo
+
+
+def atualizar_cursos(form):
+    lista_cursos = []
+    for campo in form:
+        if 'curso_' in campo.name:
+            if campo.data:  # se o campo estiver marcado
+                # adicionar o texto do campo.label.text (Excel Impressionador) na lista de cursos
+                lista_cursos.append(campo.label.text)
+    return ';'.join(lista_cursos)
+
+@app.route('/perfil/editar', methods=['GET', 'POST'])
+@login_required
+def editar_perfil():
+    form = FormEditarPerfil()
+    if form. validate_on_submit():
+        current_user.email = form.email.data
+        current_user.username = form.username.data
+        if form.foto_perfil.data:
+            nome_imagem = salvar_imagem(form.foto_perfil.data)
+            current_user.foto_perfil = nome_imagem
+        current_user.cursos = atualizar_cursos(form)
+        database.session.commit()
+        flash(f'Perfil atualizado com sucesso!', 'alert-success')
+        return redirect(url_for('perfil'))
+    elif request.method == "GET":
+        form.email.data = current_user.email
+        form.username.data = current_user.username
+    foto_perfil = url_for('static', filename=f'fotos_perfil/{current_user.foto_perfil}')
+    return render_template('editarperfil.html', foto_perfil=foto_perfil, form=form)
+
+
+@app.route('/post/<post_id>', methods=['GET', 'POST'])
+@login_required
+def exibir_post(post_id):
+    post = Post.query.get(post_id)
+    if current_user == post.autor:
+        form = FormCriarPost()
+        if request.method == 'GET':
+            form.titulo.data = post.titulo
+            form.corpo.data = post.corpo
+        elif form.validate_on_submit():
+            post.titulo = form.titulo.data
+            post.corpo = form.corpo.data
+            database.session.commit()
+            flash('Post atualizado com sucesso!', 'alert-success')
+            return redirect(url_for('home'))
+    else:
+        form = None
+    return render_template('post.html', post=post, form=form)
+
+
+@app.route('/post/<post_id>/excluir', methods=['GET', 'POST'])
+@login_required
+def excluir_post(post_id):
+    post = Post.query.get(post_id)
+    if current_user == post.autor:
+        database.session.delete(post)
+        database.session.commit()
+        flash('Post excluído com sucesso!', 'alert-danger')
+        return redirect(url_for('home'))
+    else:
+        abort(403)
